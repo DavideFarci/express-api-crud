19d26813ddface0bd24791ca8aa387bd6ad735c5
DavideFarci <yank92.df@gmail.com>
2023-11-23 16:45:31 +0100

    add errors control in postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,10 +3,16 @@ const fs = require("fs");
 const path = require("path");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const PrismaExeption = require("../exeptions/prismaExeption");
 const { slugControl } = require("../Utilities/functions");
+const { threadId } = require("worker_threads");
+
 // INDEX
 async function index(req, res) {
   const data = await prisma.post.findMany();
+  if (!data) {
+    throw new PrismaExeption("Qualcosa è andato storto, riprova", 500);
+  }
 
   return res.json(data);
 }
@@ -20,6 +26,10 @@ async function show(req, res) {
     },
   });
 
+  if (!data) {
+    throw new PrismaExeption("Qualcosa è andato storto, riprova", 500);
+  }
+
   return res.json(data);
 }
 
@@ -27,6 +37,12 @@ async function show(req, res) {
 async function store(req, res) {
   const postToCreate = req.body;
   const list = await prisma.post.findMany();
+  if (!list) {
+    throw new PrismaExeption(
+      "Non è stato possibile verificare i duplicati",
+      500
+    );
+  }
   const newPost = await prisma.post.create({
     data: {
       title: postToCreate.title,
@@ -36,14 +52,24 @@ async function store(req, res) {
     },
   });
 
+  if (!newPost) {
+    throw new PrismaExeption("Errore nella creazione del post", 500);
+  }
+
   return res.json(newPost);
 }
 
-// UPDATE
+// UPDATE (SLUG)
 async function update(req, res) {
   const { slug } = req.params;
   const postToUpdate = req.body;
   const list = await prisma.post.findMany();
+  if (!list) {
+    throw new PrismaExeption(
+      "Non è stato possibile verificare i duplicati",
+      500
+    );
+  }
   const postUpdated = await prisma.post.update({
     where: {
       slug: slug,
@@ -55,6 +81,10 @@ async function update(req, res) {
       content: postToUpdate.content,
     },
   });
+
+  if (!postUpdated) {
+    throw new PrismaExeption("Errore nella modifica del post", 500);
+  }
 
   res.json({
     message: `Il post ${postToUpdate.title} è stato modificato:`,
@@ -70,6 +100,10 @@ async function destroy(req, res) {
       slug: slug,
     },
   });
+
+  if (!postToDestroy) {
+    throw new PrismaExeption("Errore nella cancellazione del post", 500);
+  }
 
   res.json({ message: "Post eliminato correttamente!" });
 }

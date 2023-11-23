73aa3fdbf0f86abfdfcd7b22cf5a0b2c5637a553
DavideFarci <yank92.df@gmail.com>
2023-11-23 16:34:42 +0100

    add control for routeNotFound

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const postsRouter = require("./routers/postsRouter");
+const routeNotFoundMiddlware = require("./middlwares/routeNotFound");
 
 const app = express();
 app.use(express.json());
 
 // Rotte per l'entità post
 app.use("/posts", postsRouter);
+
+// Errore 404 - Pagina non trovata
+app.use(routeNotFoundMiddlware);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port http://localhost:" + process.env.PORT);
